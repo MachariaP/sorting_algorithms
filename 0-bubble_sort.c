0546ce4d15be74f1dffcad207022fe6348867a6d
MachariaP <walburphinehas78@gmail.com>
2024-01-20 12:14:32 +0300

    a function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,43 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *		- using the Bubble sort algorithm.
- * @array: The array to be sorted.
- * @size: The size of the array.
- */
+* bubble_sort - sorts an array of integers in ascending
+* order using the Bubble sort algorithm
+* @array: input array of integers
+* @size: size of array
+*/
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i;
 	int temp;
-	int swapped;
+	int swap_counter = -1;
 
-	/* Check for NULL array or array with less than 2 elements */
+	/*Array does not need to be sorted if size less than 2*/
 	if (array == NULL || size < 2)
-		return;
-
-	/* Outer loop for each pass through the array */
-	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0; /* Initialize swapped to false for each pass */
+		return;
+	}
 
-		/* Inner loop for each comparision and swap */
-		for (j = 0; j < size - i - 1; j++)
+	/*loops until swap_counter*/
+	while (swap_counter)
+	{
+		swap_counter = 0;
+		for (i = 0; i < size - 1; i++)
 		{
-			/* Compare adjacent elements and swap if there is need */
-			if (array[j] > array[j + 1])
+			/*if adjacent elements not in expected order */
+			if (array[i] > array[i + 1])
 			{
-				/* Swap array[j] and array[j + 1] */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				/*swap them */
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				/*increment swap_counter*/
+				swap_counter++;
 
-				/* print the array after each swap */
+				/*print array*/
 				print_array(array, size);
-
-				swapped = 1; /* Set swapped to true */
 			}
 		}
-
-		/* If no two elements were swapped in inner loop, the array is sorted */
-		if (swapped == 0)
-			break;
 	}
 }
